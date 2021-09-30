b8e41be9fbbb4210253ffc584e20c58c0dbe0e38
Cristopher Escorcia <cescorcia@condorlabs.io>
2021-09-30 08:33:16 -0500

    chore: adds file left behind

---
+++ PokemonApp/PokemonApp+Injection.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonApp+Injection.swift
+//  PokemonApp
+//
+//  Created by Cristopher Escorcia on 29/09/21.
+//
+
+import Foundation
+import Resolver
+
+extension Resolver: ResolverRegistering {
+    public static func registerAllServices() {
+        registerGetPokemonsFromAGeneration()
+        registerGetASinglePokemon()
+        registerGetRandomPokemon()
+        registerGetPokemondsVoted()
+        registerStorePokemon()
+    }
+}
+
