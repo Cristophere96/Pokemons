38e504a0fd02b3432edc7fc48e8c8d86fc180610
Cristopher Escorcia <cescorcia@condorlabs.io>
2021-10-11 11:25:13 -0500

    test: adds UI cases

--- PokemonAppUITests/PokemonAppUITests.swift
+++ PokemonAppUITests/PokemonAppUITests.swift
@@ -24,62 +24,71 @@ class PokemonAppUITests: XCTestCase {
     func testWhenPressedTheVotingSistemTabButtonThenNavigatesToTheVotePokemonScreen() {
         let tabBars = self.app.tabBars
         let button = tabBars.buttons["Vote for a Pokemon"]
-        
         button.tap()
-        
         self.app.wait(for: .runningBackground, timeout: 2.0)
     }
     
     func testWhenPressedTheYourVotesTabButtonThenNavigatesToThePokemonsVotedScreen() {
         let tabBars = self.app.tabBars
         let button = tabBars.buttons["Your votes"]
-        
         button.tap()
-        
         self.app.wait(for: .runningBackground, timeout: 2.0)
     }
 
     func test_navigatesToTheFirstGenerationScreen() {
         let genCell = self.app.buttons["first_gen"]
         genCell.tap()
-        
         self.app.wait(for: .runningBackground, timeout: 2.0)
     }
     
     func test_selectsAPokemonThenNavigatesToTheDetailScreen() {
         let genCell = self.app.buttons["first_gen"]
         genCell.tap()
-        
         self.app.wait(for: .runningBackground, timeout: 8.0)
-        
         let pokemon = self.app.buttons["Pokemon#1"]
         pokemon.tap()
-        
+        self.app.wait(for: .runningBackground, timeout: 2.0)
+    }
+    
+    func test_NavigatesToTheDetailScreenThenShowsThePokemonStats() {
+        let genCell = self.app.buttons["first_gen"]
+        genCell.tap()
+        self.app.wait(for: .runningBackground, timeout: 8.0)
+        let pokemon = self.app.buttons["Pokemon#1"]
+        pokemon.tap()
+        self.app.wait(for: .runningBackground, timeout: 2.0)
+        let stats = self.app.buttons["Stats"]
+        stats.tap()
+        self.app.wait(for: .runningBackground, timeout: 2.0)
+    }
+    
+    func test_NavigatesToTheDetailScreenThenShowsThePokemonMoves() {
+        let genCell = self.app.buttons["first_gen"]
+        genCell.tap()
+        self.app.wait(for: .runningBackground, timeout: 8.0)
+        let pokemon = self.app.buttons["Pokemon#1"]
+        pokemon.tap()
+        self.app.wait(for: .runningBackground, timeout: 2.0)
+        let moves = self.app.buttons["Moves"]
+        moves.tap()
         self.app.wait(for: .runningBackground, timeout: 2.0)
     }
     
     func testNavigatesToDetailScreenFromVotePokemonScreen() {
         let tabBars = self.app.tabBars
         let button = tabBars.buttons["Vote for a Pokemon"]
-        
         button.tap()
-        
         self.app.wait(for: .runningBackground, timeout: 2.0)
-        
         let pokemon = self.app.buttons["largePokemonCell"]
         pokemon.tap()
-        
         self.app.wait(for: .runningBackground, timeout: 2.0)
     }
     
     func testVotesAPokemonWithLiked() {
         let tabBars = self.app.tabBars
         let button = tabBars.buttons["Vote for a Pokemon"]
-        
         button.tap()
-        
         self.app.wait(for: .runningBackground, timeout: 2.0)
-        
         let pokemon = self.app.buttons["largePokemonCell"]
         pokemon.swipeRight()
     }
@@ -87,40 +96,19 @@ class PokemonAppUITests: XCTestCase {
     func testVotesAPokemonWithDisliked() {
         let tabBars = self.app.tabBars
         let button = tabBars.buttons["Vote for a Pokemon"]
-        
         button.tap()
-        
         self.app.wait(for: .runningBackground, timeout: 2.0)
-        
         let pokemon = self.app.buttons["largePokemonCell"]
         pokemon.swipeLeft()
-    }
-    
-    func testNavigatesToDetailScreenFromPokemonsVotedScreen() {
-        let tabBars = self.app.tabBars
-        let button = tabBars.buttons["Your votes"]
-        
-        button.tap()
-        
-        self.app.wait(for: .runningBackground, timeout: 2.0)
-        
-        let pokemon = self.app.buttons["Pokemon#8"]
-        pokemon.tap()
-        
-        self.app.wait(for: .runningBackground, timeout: 2.0)
     }
     
     func testShowsTheDislikedPokemons() {
         let tabBars = self.app.tabBars
         let button = tabBars.buttons["Your votes"]
-        
         button.tap()
-        
         self.app.wait(for: .runningBackground, timeout: 2.0)
-        
         let tab = self.app.buttons["Disliked"]
         tab.tap()
-        
         self.app.wait(for: .runningBackground, timeout: 2.0)
     }
 }
