464ef97523cef764f46115cbb1c9a14de7efca13
Cristopher Escorcia <cescorcia@condorlabs.io>
2021-10-19 09:18:21 -0500

    fix: fixes constraints alert on debut console

--- PokemonApp/Views/Generations/GenerationsView.swift
+++ PokemonApp/Views/Generations/GenerationsView.swift
@@ -35,6 +35,7 @@ struct GenerationsView: View {
                     .navigationTitle("Pokedex")
                 }
             }
+            .navigationViewStyle(StackNavigationViewStyle())
         }
     }
 }
--- PokemonApp/Views/VotingSystem/PokemonsVotedView.swift
+++ PokemonApp/Views/VotingSystem/PokemonsVotedView.swift
@@ -52,6 +52,7 @@ struct PokemonsVotedView: View {
                 viewModel.fetchPokemons()
             }
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
